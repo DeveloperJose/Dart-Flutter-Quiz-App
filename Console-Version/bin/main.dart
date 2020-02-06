deaff012e036f6f62edcee834e19d3169d85089c
DeveloperJose <josegperez@mail.com>
2020-02-06 15:32:14 -0700

    Added initial files for console version

---
+++ Console-Version/bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:Console_Version/Console_Version.dart' as Console_Version;
+
+void main(List<String> arguments) {
+  print('Hello world: ${Console_Version.calculate()}!');
+}
---
+++ Console-Version/test/Console_Version_test.dart
@@ -0,0 +1,8 @@
+import 'package:Console_Version/Console_Version.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
