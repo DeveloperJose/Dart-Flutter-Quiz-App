7a91a5e84610c6d9b451eeac493bc32dc7383e49
DeveloperJose <josegperez@mail.com>
2020-03-31 03:30:25 -0600

    Log-in page layout finished

--- quiz_app/lib/login_page.dart
+++ quiz_app/lib/login_page.dart
@@ -1,4 +1,5 @@
 // Author: Jose G. Perez
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatefulWidget {
@@ -9,15 +10,21 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+
+
+  void onSignInPressed(){
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Login Page'),
       ),
-      body: Center(
+      body: Container(
+        padding: EdgeInsets.all(30.0),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             this.buildFormWidget(context),
           ],
@@ -26,32 +33,60 @@ class _LoginPageState extends State<LoginPage> {
     );
   }
 
-  Widget buildFormWidget(BuildContext context){
-    return Form(child: Column(children: [
-      buildUsernameWidget(),
-      buildPasswordWidget(),
-      buildSubmitWidget(),
-    ]));
-  }
-
-  Widget buildUsernameWidget(){
-    return TextFormField(
-      keyboardType: TextInputType.text,
-      decoration: InputDecoration(hintText: 'utep', labelText: 'Username'),
+  Widget buildFormWidget(BuildContext context) {
+    return Form(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          buildDescriptionWidget(),
+          buildUsernameWidget(),
+          buildPasswordWidget(),
+          buildSubmitWidget(),
+          buildHintTextWidget(),
+        ],
+      ),
     );
   }
 
-  Widget buildPasswordWidget(){
+  Widget buildDescriptionWidget() {
+    return Center(
+      child: Text(
+        "CS 4381",
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 48,
+          color: Colors.green,
+        ),
+      ),
+    );
+  }
+
+  Widget buildUsernameWidget() {
+    return TextFormField(
+      keyboardType: TextInputType.text,
+      decoration: InputDecoration(hintText: 'username', labelText: 'Username*', icon: Icon(Icons.person)),
+    );
+  }
+
+  Widget buildPasswordWidget() {
     return TextFormField(
       keyboardType: TextInputType.text,
       obscureText: true,
-      decoration: InputDecoration(hintText: 'password', labelText: 'Password'),
+      decoration: InputDecoration(hintText: 'password', labelText: 'Password', icon: Icon(Icons.lock)),
     );
   }
 
-  Widget buildSubmitWidget(){
+  Widget buildSubmitWidget() {
     return RaisedButton(
-      child: Text('Log In')
+      child: Text('Sign In'),
+      onPressed: onSignInPressed,
     );
+  }
+
+  Widget buildHintTextWidget() {
+    return Text(
+        '*User name of your primary email address provided to by UTEP, typically your @miners email address',
+        style: TextStyle(fontSize: 10));
   }
 }
